c9d79961fa7a736a7489a5fad59092524ceaeef5
Lawrence Barrett <91626403+LJB0695@users.noreply.github.com>
2023-01-11 15:10:34 -0500

    Add files via upload
    Finished project 1.

---
+++ Project1/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are in a land full of dragons. In front of you, \n" +
+                "you see two caves. In one cave, the dragon is friendly \n" +
+                "and will share his treasure with you. The other dragon \n" +
+                "is greedy and hungry and will eat you on sight. \n" +
+                "Which cave will you go into? (1 or 2)\n");
+
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("You approach the cave... \n " +
+                    "It is dark and spooky... \n" +
+                    "A large dragon jumps out in front of you! He opens his jaws and... \n" +
+                    "Gobbles you down in one bite! You picked the wrong cave! \n");
+        } else if (choice == 2) {
+            System.out.println("You approach the cave... \n " +
+                    "It is dark and spooky... \n" +
+                    "A large dragon jumps out in front of you! He opens his jaws and... \n" +
+                    "Offers to share their gold with you! You picked the right cave! \n");
+        } else {
+            System.out.println("Please choose either a 1 or a 2.");
+        }
+    }
+}
